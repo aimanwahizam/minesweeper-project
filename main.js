373c3696097f6c3de0e58761eeb8316124c800db
aiman.wahizam <aiman.wahizam@nology.io>
2022-05-26 15:24:02 +0100

    click on one box is working. when start wont work

--- main.js
+++ main.js
@@ -2,20 +2,40 @@
 /*                                HTML Elements                               */
 /* -------------------------------------------------------------------------- */
 
-const gameBox = document.querySelectorAll(".game__box"); // ARRAY OF BOXES
+const body = document.querySelector("body")
+const game = document.querySelector(".game");
+const gameBox = document.querySelectorAll(".game__box") // ARRAY OF BOXES TO BE STORED
+const startButton = document.querySelector(".start-button");
 
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
+const onGameStart = () => {
+  for (let index = 1; index < (16 ** 2) + 1; index++) {
+    game.innerHTML += `<div class="game__box" id="${index}"></div>`;
+  }
+};
+
+// const updateGameBox = () => {
+//   gameBox = document.querySelectorAll(".game__box"); 
+//   console.log(gameBox);
+// }
+
 const onClickGameBox = (event) => {
-    event.target.setAttribute("class", "game__box--click");
-}
+  event.target.setAttribute("class", "game__box--click");
+  console.log(event.target);
+};
 
 /* -------------------------------------------------------------------------- */
 /*                               Event Listeners                              */
 /* -------------------------------------------------------------------------- */
 
-gameBox.forEach(box => {
-    box.addEventListener("click", onClickGameBox);
+startButton.addEventListener("click", onGameStart);
+
+gameBox.forEach((box) => {
+  box.addEventListener("click", onClickGameBox);
 });
+
+// body.addEventListener("change", updateGameBox)
+
