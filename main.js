2a68eab3399b676cfffe03e52dc8861a16c05c76
aiman.wahizam <aiman.wahizam@nology.io>
2022-05-26 16:40:17 +0100

    made random number generator to assign bombs

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ const game = document.querySelector(".game");
 const gameBox = [] // ARRAY OF BOXES TO BE STORED
 const startButton = document.querySelector(".start-button");
 
+const randomNumberArray = [];
+
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
-const gameBoxMemory = (array) => {
+const addGameBoxEventListeners = (array) => {
   array.forEach((box) => {
     box.addEventListener("click", onClickGameBox);
   });
@@ -21,12 +23,32 @@ const onGameStart = () => {
   for (let index = 1; index < (16 ** 2) + 1; index++) {
     game.innerHTML += `<div class="game__box" id="${index}"></div>`;
   }
-  gameBoxMemory(document.querySelectorAll(".game__box"));
+  addGameBoxEventListeners(document.querySelectorAll(".game__box"));
 
   document.querySelectorAll(".game__box").forEach(box => {
     gameBox.push(box);
   })
+  console.log(gameBox)
 }
+
+const generateRandomNumbersArray = (numberOfBombs, numberOfRows, array) => {
+  // generate an array of numberOfBombs random numbers from 1 - numberofRows**2
+  // make sure no two same random numbers  
+  while (array.length < numberOfBombs) {
+    const randomNumber = Math.floor(Math.random() * (numberOfRows ** 2))
+
+    // Check if unique
+    array.indexOf(randomNumber) === -1 ? array.push(randomNumber) : null;
+  }
+  console.log(array);
+  return array;
+}
+
+// const assignBombs = () => {
+  // foreach gamebox if id matches random number ADD class of bomb
+  // call randomnumberarray function
+
+// }
 
 const onClickGameBox = (event) => {
   event.target.setAttribute("class", "game__box--click");
@@ -38,5 +60,6 @@ const onClickGameBox = (event) => {
 /* -------------------------------------------------------------------------- */
 
 startButton.addEventListener("click", onGameStart);
+startButton.addEventListener("click", generateRandomNumbersArray(32, 16, randomNumberArray))
 
 
